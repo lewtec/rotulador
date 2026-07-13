32c396c9286a307531ff0d87c17bacf8b301c4d0
lucasew <lucas59356@gmail.com>
2026-07-13 21:52:03 +0000

    fix(image): clean up temp files on ingest failure paths
    IngestImage leaked the UUID temp PNG when encode or close failed, and used
    path.Join for filesystem paths. Close+remove temps on error, use filepath,
    and cover happy-path and failed-output-dir cases with tests.

--- annotation/image.go
+++ annotation/image.go
@@ -4,55 +4,66 @@ import (
 	"context"
 	"crypto/sha256"
 	"fmt"
-	"github.com/google/uuid"
 	"image"
 	_ "image/gif"
 	_ "image/jpeg"
 	"image/png"
 	"io"
 	"os"
-	"path"
+	"path/filepath"
+
+	"github.com/google/uuid"
 )
 
-func DecodeImage(filepath string) (image.Image, error) {
-	f, err := os.Open(filepath)
+func DecodeImage(path string) (image.Image, error) {
+	f, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer func() {
 		if err := f.Close(); err != nil {
-			ReportError(context.TODO(), err, "msg", "failed to close image file", "path", filepath)
+			ReportError(context.Background(), err, "msg", "failed to close image file", "path", path)
 		}
 	}()
 	m, _, err := image.Decode(f)
 	if err != nil {
 		return nil, err
 	}
-	return m, err
+	return m, nil
 }
 
 func IngestImage(img image.Image, outputDir string) error {
-	tempFile := path.Join(outputDir, fmt.Sprintf("%s.png", uuid.New()))
+	tempFile := filepath.Join(outputDir, fmt.Sprintf("%s.png", uuid.New()))
 	f, err := os.Create(tempFile)
 	if err != nil {
 		return err
 	}
+
 	hasher := sha256.New()
 	w := io.MultiWriter(f, hasher)
-	err = png.Encode(w, img)
-	if err != nil {
-		return err
-	}
-	err = f.Close()
-	if err != nil {
-		return err
-	}
-	err = os.Rename(tempFile, path.Join(outputDir, fmt.Sprintf("%x.png", hasher.Sum(nil))))
-	if err != nil {
+	if err := png.Encode(w, img); err != nil {
+		if closeErr := f.Close(); closeErr != nil {
+			ReportError(context.Background(), closeErr, "msg", "failed to close temp image after encode error", "path", tempFile)
+		}
 		if removeErr := os.Remove(tempFile); removeErr != nil {
-			ReportError(context.TODO(), removeErr, "msg", "failed to remove temp file", "path", tempFile)
+			ReportError(context.Background(), removeErr, "msg", "failed to remove temp file after encode error", "path", tempFile)
 		}
 		return err
 	}
-	return err
+
+	if err := f.Close(); err != nil {
+		if removeErr := os.Remove(tempFile); removeErr != nil {
+			ReportError(context.Background(), removeErr, "msg", "failed to remove temp file after close error", "path", tempFile)
+		}
+		return err
+	}
+
+	finalPath := filepath.Join(outputDir, fmt.Sprintf("%x.png", hasher.Sum(nil)))
+	if err := os.Rename(tempFile, finalPath); err != nil {
+		if removeErr := os.Remove(tempFile); removeErr != nil {
+			ReportError(context.Background(), removeErr, "msg", "failed to remove temp file", "path", tempFile)
+		}
+		return err
+	}
+	return nil
 }
